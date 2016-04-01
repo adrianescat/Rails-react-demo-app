8ced0ec8e3511481b279593a0cc6beef1b898664
adrian.escat <adrian.escat@dev.redmintlabs.com>
2016-04-01 12:38:21 -0300

    identation

--- app/assets/javascripts/components/records.js
+++ app/assets/javascripts/components/records.js
@@ -61,42 +61,67 @@ this.Records = React.createClass({
   },
   render: function() {
     var record;
-    return React.DOM.div({
-      className: 'records'
-    }, React.DOM.h2({
-      className: 'title'
-    }, 'Records'), React.DOM.div({
-      className: 'row'
-    }, React.createElement(AmountBox, {
-      type: 'success',
-      amount: this.credits(),
-      text: 'Credit'
-    }), React.createElement(AmountBox, {
-      type: 'danger',
-      amount: this.debits(),
-      text: 'Debit'
-    }), React.createElement(AmountBox, {
-      type: 'info',
-      amount: this.balance(),
-      text: 'Balance'
-    })), React.createElement(RecordForm, {
-      handleNewRecord: this.addRecord
-    }), React.DOM.hr(null),React.DOM.table({
-      className: 'table table-bordered'
-    }, React.DOM.thead(null, React.DOM.tr(null, React.DOM.th(null, 'Date'), React.DOM.th(null, 'Title'), React.DOM.th(null, 'Amount'),React.DOM.th(null, 'Actions'))), React.DOM.tbody(null, (function() {
-      var i, len, ref, results;
-      ref = this.state.records;
-      results = [];
-      for (i = 0, len = ref.length; i < len; i++) {
-        record = ref[i];
-        results.push(React.createElement(Record, {
-          key: record.id,
-          record: record,
-          handleDeleteRecord: this.deleteRecord,
-          handleEditRecord: this.updateRecord
-        }));
-      }
-      return results;
-    }).call(this))));
+    return React.DOM.div(
+      {
+        className: 'records'
+      }, 
+      React.DOM.h2(
+        {
+          className: 'title'
+        }, 'Records'), 
+      React.DOM.div(
+        {
+          className: 'row'
+        }, 
+        React.createElement(AmountBox, {
+          type: 'success',
+          amount: this.credits(),
+          text: 'Credit'
+        }), 
+        React.createElement(AmountBox, {
+          type: 'danger',
+          amount: this.debits(),
+          text: 'Debit'
+        }), 
+        React.createElement(AmountBox, {
+          type: 'info',
+          amount: this.balance(),
+          text: 'Balance'
+        })
+      ), 
+        React.createElement(RecordForm, {
+          handleNewRecord: this.addRecord
+        }), 
+        React.DOM.hr(null),
+        React.DOM.table(
+          {
+            className: 'table table-bordered'
+          }, 
+          React.DOM.thead(null, 
+            React.DOM.tr(null, 
+              React.DOM.th(null, 'Date'), 
+              React.DOM.th(null, 'Title'), 
+              React.DOM.th(null, 'Amount'),
+              React.DOM.th(null, 'Actions')
+            )
+          ), 
+          React.DOM.tbody(null, (function() {
+            var i, len, ref, results;
+            ref = this.state.records;
+            results = [];
+            for (i = 0, len = ref.length; i < len; i++) {
+              record = ref[i];
+              results.push(React.createElement(Record, {
+                key: record.id,
+                record: record,
+                handleDeleteRecord: this.deleteRecord,
+                handleEditRecord: this.updateRecord
+              }));
+            }
+            return results;
+          }).call(this)
+          )
+        )
+    );
   }
 });
